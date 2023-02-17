ee0543056fbdf82c8a73b15a5ba0643eb659ac0e
Jenny Carroll <jennygc@telus.net>
2023-02-17 23:12:42 +0000

    initial commit, we have an class with a constructor and a method

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let balance = 500.00;
+// Allow multiple accounts to be created
+// Each account can have many transactions
+// Allow withdrawals and deposits into accounts
+// Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+// Allow us to retrieve the current balance of the account at any time
+// Don't allow withdrawals that exceed the remaining balance of the account
+
+let balance = 500.0;
 
 class Withdrawal {
-
   constructor(amount) {
     this.amount = amount;
   }
@@ -9,21 +15,17 @@ class Withdrawal {
   commit() {
     balance -= this.amount;
   }
-
 }
-
-
-
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
 t1 = new Withdrawal(50.25);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log("Transaction 1:", t1);
 
 t2 = new Withdrawal(9.99);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Transaction 2:", t2);
 
-console.log('Balance:', balance);
+console.log("Balance:", balance);

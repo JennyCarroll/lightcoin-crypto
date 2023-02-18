8fa661c9ee3cbf6f9313d3fcd5d64df3ae76dc32
Jenny Carroll <jennygc@telus.net>
2023-02-18 01:05:06 +0000

    create classes and instances of them and use getters and output account info...

--- index.js
+++ index.js
@@ -5,27 +5,96 @@
 // Allow us to retrieve the current balance of the account at any time
 // Don't allow withdrawals that exceed the remaining balance of the account
 
-let balance = 500.0;
+// let balance = 500.0;
 
-class Withdrawal {
-  constructor(amount) {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+    // Have the account balance start at $0 since that makes more sense.
+    // this.balance = 0;
+  }
+  get balance() {
+    // Calculate the balance using the transaction objects.
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    ///account is an object with a balance property and the withdrawa outputs a value
+    // this.account.balance += this.value;
+    this.account.addTransaction(this);
+    return true;
   }
 
-  commit() {
-    balance -= this.amount;
+  toString() {
+    return `This transaction is for ${this.amount}`;
+  }
+}
+
+class Withdrawal extends Transaction {
+  // constructor(amount, account) {
+  //   this.amount = amount;
+  //   this.account = account;
+  // }
+  get value() {
+    // if ()
+    return -this.amount;
+  }
+  isAllowed() {
+    //note how it has access to this.account b/c of parent
+    return this.account.balance - this.amount >= 0;
+  }
+}
+
+class Deposit extends Transaction {
+  // constructor(amount, account) {
+  //   this.amount = amount;
+  //   this.account = account;
+  // }
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
   }
 }
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+console.log("Starting Balance:", myAccount.balance);
+
+t3 = new Deposit(120.0, myAccount);
+t3.commit();
+console.log("Transaction 3:", t3);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log("Transaction 1:", t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log("Transaction 2:", t2);
 
-console.log("Balance:", balance);
+for (let x of myAccount.transactions) {
+  console.log(x.toString());
+}
+
+console.log("Ending Balance:", myAccount.balance);
+console.log("my balance:", myAccount.balance);
